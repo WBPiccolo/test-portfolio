0bd2d1a3dc294c5369cbe80080501fb3aa867ff5
francesco.piccolo <francesco.piccolo@lynxspa.com>
2021-10-06 18:15:49 +0200

    prima bozza componente

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -8,11 +8,26 @@ import { Portfolio } from 'src/app/shared/utilities';
 })
 export class PortfolioComponent implements OnInit {
   @Input()
-  portfolio: Portfolio | undefined;
-
+  portfolio: any;
+  icons = [
+    { id: '1', icon: '', showText: 'Test icona 1' },
+    { id: '2', icon: '', showText: 'Test icona 2' },
+    { id: '3', icon: '', showText: 'Test icona 3' },
+    { id: '4', icon: '', showText: 'Test icona 4' },
+    { id: '5', icon: '', showText: 'Test icona 5' },
+    { id: '6', icon: '', showText: 'Test icona 6' },
+  ];
+  text = '';
+  value = '';
   constructor() {}
 
   ngOnInit(): void {
     console.log(this.portfolio);
+  }
+
+  showInfo(icon: any) {
+    console.log(icon);
+    this.text = icon.showText;
+    this.value = icon.id;
   }
 }
